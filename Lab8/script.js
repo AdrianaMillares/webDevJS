5e479c847e75f6be652c37d2ead5b833a831290d
AdriMillares <a01705674@itesm.mx>
2021-02-22 16:32:37 -0600

    Avance lab8

--- Lab8/script.js
+++ Lab8/script.js
@@ -1,12 +1,26 @@
+// Funciones
+
+//Funcion 1
+function problema1(){
+    const tam = window.prompt('inserte el tamaño de su arreglo');
+	let arreglo = [];
+    let promedio = 0;
+
+	for(let i = 1; i <= tam; i++){
+        arreglo[i] = window.prompt('inserte el numero ' + i +  'de su arreglo');
+        promedio = promedio + arreglo[i];
+    }
+    promedio = promedio / tam;    
+}
+
 // server .js
 
 'use strict';
 const http = require('http');
-const  server  = require('https');
 
-constserver = http.createServer(function(req, res){
+const server = http.createServer(function(req, res){
     res.writeHead(200, {'content-type': 'text/plain'});
-    res.end('Hola Mundo');
+    res.end('Pregunta 1' + problema1());
 });
 
 server.listen(8000);
