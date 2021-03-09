2ab10719aa5c14c97e99f9156879116b3be3f7c4
AdriMillares <a01705674@itesm.mx>
2021-03-08 22:25:43 -0600

    LogIn y LogOut

--- Lab14/controllers/peliculas_controller.js
+++ Lab14/controllers/peliculas_controller.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const Peliculas = require('../models/peliculas');
+
 Peliculas.fetchAll().forEach((pelicula) => {
     fs.appendFileSync("pelicula.txt", Peliculas + "\n");
 });
@@ -17,11 +18,11 @@ exports.postNuevaPelicula = (request, response, next) => {
     const nueva_pelicula = new Peliculas(request.body.nombre);
     nueva_pelicula.save();
     fs.appendFileSync("peliculas.txt", request.body.nombre + "\n");
-    response.setHeader('Set-Cookie', 'ultima_pelicula=' +nueva_pelicula.nombre);
+    response.setHeader('Set-Cookie', 'ultima_pelicula=' +nueva_pelicula.nombre+ '; HttpOnly');
     response.redirect("/peliculas");
 };
 
 exports.getpelicula = (request, response, next) => {
-    console.log(request.get('Cookie'))
+    console.log(request.cookies);
     response.render('peliculas', { lista_peliculas: Peliculas.fetchAll() });
 };
